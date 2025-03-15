b5215d2f028d0f0bd12b8e4b433edce0eee7c58e
Shark <39524175+498777@users.noreply.github.com>
2025-03-15 15:11:15 +0800

    Update miscs.ts

--- src/restful/miscs.ts
+++ src/restful/miscs.ts
@@ -29,7 +29,7 @@ export default async function register($app: Hono) {
     });
 
     // Redirect sub.store to vercel webpage
-    $app.get('/', async (c: Context) => c.redirect('https://sub.r2s.site/', 302));
+    $app.get('/', async (c: Context) => c.redirect('https://sub-store-workers.vercel.app/', 302));
 
     $app.all('/', (c: Context) => {
         return c.text('Hello from sub-store, made with ❤️ by Peng-YM');
